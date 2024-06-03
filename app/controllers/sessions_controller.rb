4eb067dcf5e16d30c826b38873f479648ed8f282
ostalean ostale <antonino.ostale@kellogg.northwestern.edu>
2024-06-03 21:27:09 +0000

    correct user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by({"email" => params["email"]})
 
+    if @user != nil
 
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
@@ -15,6 +16,11 @@ class SessionsController < ApplicationController
         redirect_to "/login"
   end
 
+else
+  # 4a. if the user doesn't exist -> login fails
+  flash["notice"] = "Incorrect User"
+  redirect_to "/login"
+end
 end
   def destroy
     # logout the user
