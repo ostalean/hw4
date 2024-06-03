65200e8185478edb27f0801643f796d30635f356
ostalean ostale <antonino.ostale@kellogg.northwestern.edu>
2024-06-03 22:18:44 +0000

    with entries for loged in

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,20 @@ class EntriesController < ApplicationController
   end
 
   def create
+    if User.find_by({ "id" => session["user_id"] }) != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
     @entry.save
+
     redirect_to "/places/#{@entry["place_id"]}"
+  else
+    flash["notice"] = "You have to be logged in."
+    redirect_to "/login"
+  end
   end
 
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,20 +2,29 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    if @place
+      # Fetch entries for the specific place and user
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
+    else
+      flash["notice"] = "Place not found."
+      redirect_to "/places"
+    end
   end
 
   def new
   end
 
   def create
+    if User.find_by({ "id" => session["user_id"] }) != nil
     @place = Place.new
     @place["name"] = params["name"]
     @place.save
+  end
     redirect_to "/places"
   end
 
